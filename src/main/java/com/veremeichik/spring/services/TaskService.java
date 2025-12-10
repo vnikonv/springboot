51e8470343403ec7259f234f35036390be0b77e5
Nikon Veremeichik <thesecondnikon@gmail.com>
2025-12-10 17:36:44 +0500

    Make the mapping work properly, change the presentation of users and tasks, improve project structure

---
+++ src/main/java/com/veremeichik/spring/services/TaskService.java
@@ -0,0 +1,16 @@
+package com.veremeichik.spring;
+
+import com.veremeichik.spring.entities.Task;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class TaskService {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+}
